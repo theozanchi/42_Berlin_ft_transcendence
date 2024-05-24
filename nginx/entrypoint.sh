94670efda98db2518b43122aa29aa9c75c465b91
tzanchi <tzanchi@student.42berlin.de>
2024-05-24 14:43:44 +0200

    pushed nginx config elsewhere

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -3,7 +3,7 @@
 envsubst < /tmp/nginx.conf > /tmp/nginx.conf.tmp
 
 mkdir -p /etc/nginx/templates
-mv /tmp/nginx.conf.tmp /etc/nginx/templates/nginx.conf
+mv /tmp/nginx.conf.tmp /etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
 
