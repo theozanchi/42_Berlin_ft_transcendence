f9ba0dac6f84a0aad745214da8a4cf16702e3c7e
esther <esthers@live.dk>
2024-05-27 17:47:36 +0200

    added tournament generating function\nImports not working properly

--- backend/game_config_service/game_config/views.py
+++ backend/game_config_service/game_config/views.py
@@ -3,8 +3,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class GameConfigAPIView(APIView):
-    def get(self, request):
-        # Your logic to retrieve game configuration data
-        return Response({'message': 'Game configuration data'})
+from django.http import HttpResponse
+from rest_framework.views import APIView
 
+from .tournament import generate_tournament_tree, serialize_tree
+
+class GameConfigAPIView(APIView):
+    def post(self, request):
+        # Access form data using request.data
+        game_type = request.data.get('game-type')
+        player_count = request.data.get('player-count')
+        players = [request.data.get(f'player-{i}') for i in range(1, int(player_count) + 1)]
+        
+        response = {'message': 'POST request received', 'game_type': game_type, 'player_count': player_count, 'players': players}
+        
+        if player_count > 2: 
+            tree = serialize_tree(generate_tournament_tree(player_count, players))
+            response.update(tree)
+
+        return Response(response)
