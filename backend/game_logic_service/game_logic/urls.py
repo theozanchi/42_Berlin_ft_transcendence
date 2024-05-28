1f0a2723195f0101fce1326a12b13fa61704397f
esther <esthers@live.dk>
2024-05-28 19:12:49 +0200

    api and models for game_config service

--- backend/game_logic_service/game_logic/urls.py
+++ backend/game_logic_service/game_logic/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from .views import GameLogicAPIView
+from django.contrib import admin
+from django.urls import path, include
+from .views import GameConfigAPIView
 
 urlpatterns = [
-    path('api/game-logic/', GameLogicAPIView.as_view(), name='game-logic-api'),
+    path('admin/', admin.site.urls),
+    path('', include('api.urls')),
 ]
