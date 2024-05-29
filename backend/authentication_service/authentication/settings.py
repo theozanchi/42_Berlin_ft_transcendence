0ddbc1934dce25ff59eccef14ee7e39d9f98cc83
tzanchi <tzanchi@student.42berlin.de>
2024-05-29 10:50:20 +0200

    extracted static files to share them between containers

--- backend/authentication_service/authentication/settings.py
+++ backend/authentication_service/authentication/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/usr/share/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
