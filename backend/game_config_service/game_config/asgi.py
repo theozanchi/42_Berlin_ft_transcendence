ace1d8e66d500b191ec86308b2bc74f3eec78daa
esther <esthers@live.dk>
2024-05-29 18:31:31 +0200

    updated game_config models

--- backend/game_config_service/game_config/asgi.py
+++ backend/game_config_service/game_config/asgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'game_config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter([
+    # you can define all your routers here
+        ])
+})
