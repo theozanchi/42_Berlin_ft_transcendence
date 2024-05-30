22cb6247f20f5e42d72d6101c3eaf510fffe87fb
esther <esthers@live.dk>
2024-05-30 19:42:46 +0200

    created game_lobby service

---
+++ backend/game_lobby_service/base/consumers.py
@@ -0,0 +1,45 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class LobbyConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.lobby_id = self.scope['url_route']['kwargs']['lobby_id']
+        self.lobby_group_name = f'lobby_{self.lobby_id}'
+
+        # Join lobby group
+        await self.channel_layer.group_add(
+            self.lobby_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave lobby group
+        await self.channel_layer.group_discard(
+            self.lobby_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+
+        # Send message to lobby group
+        await self.channel_layer.group_send(
+            self.lobby_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    # Receive message from lobby group
+    async def chat_message(self, event):
+        message = event['message']
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
---
+++ backend/game_manager_service/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Make migrations"
+python manage.py makemigrations
+
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start the Gunicorn server
+echo "Starting Gunicorn"
+exec gunicorn --workers 3 --bind 0.0.0.0:8001 game_manager.wsgi:application
