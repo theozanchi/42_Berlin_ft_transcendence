4e0e7b36ca1d43727860ace2947b2a0387611ba7
esther <esthers@live.dk>
2024-05-30 12:56:36 +0200

    changes made to Round model

--- backend/game_config_service/api/views.py
+++ backend/game_config_service/api/views.py
@@ -5,19 +5,23 @@ from rest_framework import status
 from base.models import Game
 
 @api_view(['POST'])
-def postGame(request):
+def create_game(request):
     # Access form data using request.data
     game = Game.objects.create(mode=request.data.get('game-mode'))
     # Create players for the game
     game.add_players_to_game(request.data)
     # Generate rounds for the game
     game.create_rounds()
+    # Game can now be played
+    #for round in game.rounds.all():
+    #    round.initialize_round()
+    #game.save()
 
     response_data = {
         'game_id': game.id, 
         'game_mode': game.mode,
-        'rounds': game.rounds.all,
         'players': [],
+        'rounds': [],
         # Add more data as needed
     }
 
@@ -26,6 +30,14 @@ def postGame(request):
         response_data['players'].append({
             'alias': player.alias,
         })
+
+    # Populate round information
+    for round_instance in game.rounds.all():  # Assuming game.rounds is a related manager to Round model
+        response_data['rounds'].append({
+            'round_number': round_instance.round_number,
+            'player1': round_instance.player1,
+            'player2': round_instance.player2,
+        })
     
-    # Game can now initialize itself by calling game.initialize_rounds
+
     return JsonResponse(response_data, status=200)
