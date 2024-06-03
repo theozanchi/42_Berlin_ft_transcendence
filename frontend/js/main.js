010da2e234c817a5dcea46a4b3c897a7ce9b8ab8
Jakob Schott <jschott@student.42berlin.de>
2024-06-03 18:34:14 +0200

    SAP proof of concept

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -9,23 +9,33 @@ document.getElementById('backButton').addEventListener('click', function() {
 });
 
 document.getElementById('addPlayerButton').addEventListener('click', function() {
-    var playerList = document.getElementById('playerList');
-    var playerDiv = document.createElement('div');
-    var input = document.createElement('input');
-    var removeButton = document.createElement('button');
+    let playerList = document.getElementById('playerList');
+    let playerDiv = document.createElement('div');
+    playerDiv.className = 'row'; // Bootstrap class for a row
 
+    var inputDiv = document.createElement('div');
+    inputDiv.className = 'col'; // Bootstrap class for a column
+    let input = document.createElement('input');
     input.type = 'text';
-    input.placeholder = 'Player name';
+    input.className = 'form-control';
+    input.placeholder = `Player #${playerList.childElementCount}`;
+    inputDiv.appendChild(input);
+
+    const buttonDiv = document.createElement('div');
+    buttonDiv.className = 'col-auto'; // Bootstrap class for a column that only takes the space it needs
+    const removeButton = document.createElement('button');
     removeButton.textContent = 'X';
+    removeButton.className = 'btn btn-outline-danger';
     removeButton.addEventListener('click', function() {
         playerList.removeChild(playerDiv);
     });
+    buttonDiv.appendChild(removeButton);
 
-    playerDiv.appendChild(input);
-    playerDiv.appendChild(removeButton);
+    playerDiv.appendChild(inputDiv);
+    playerDiv.appendChild(buttonDiv);
     playerList.appendChild(playerDiv);
 });
 
 document.getElementById('generateGameButton').addEventListener('click', function() {
-    console.log('Game generated');
+    alert(`Generating Game with ${playerList.childElementCount} Players`);
 });
