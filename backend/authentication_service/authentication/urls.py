230ff874e443530cd936a9be75393f5eb55a2dbd
Théo Zanchi <tzanchi@student.42berlin.de>
2024-06-10 17:58:31 +0200

    created first version of user management api: get and post

--- backend/authentication_service/authentication/urls.py
+++ backend/authentication_service/authentication/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include # type: ignore
 from . import views
 
 urlpatterns = [
-    path('authentication/', views.hello_world),
-    path('authentication/test', views.hello_world),
+    path('authentication/', views.user_list),
 ]
--- backend/authentication_service/authentication/views.py
+++ backend/authentication_service/authentication/views.py
@@ -1,7 +1,26 @@
+from django.http import HttpResponse, JsonResponse # type: ignore
+from rest_framework import status # type: ignore
 from rest_framework.response import Response # type: ignore
 from rest_framework.decorators import api_view # type: ignore
+from rest_framework.parsers import JSONParser # type: ignore
+from authentication.models import User
+from authentication.serializers import UserSerializer
 
 
-@api_view(['GET'])
-def hello_world(request):
-	return Response({"message": "Hello, world!"})
+@api_view(['GET', 'POST'])
+def user_list(request):
+	"""
+	List all users, or add a new user
+	"""
+	if request.method == 'GET':
+		users = User.objects.all()
+		serializer = UserSerializer(users, many=True)
+		return JsonResponse(serializer.data, safe=False)
+
+	elif request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = UserSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+		return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
