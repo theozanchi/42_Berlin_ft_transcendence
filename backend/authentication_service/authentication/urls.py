708d4526f61dbdecc1d5c6eb5b9b2c044bc13b85
Théo Zanchi <tzanchi@student.42berlin.de>
2024-06-10 15:26:50 +0200

    modified config to add manual ip to all container and set nginx as a reverse proxy

--- backend/authentication_service/authentication/urls.py
+++ backend/authentication_service/authentication/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authentication/', views.coucou),
 	path('', views.hello_world),
 ]
--- backend/authentication_service/authentication/views.py
+++ backend/authentication_service/authentication/views.py
@@ -1,7 +1,12 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+
 @api_view(['GET'])
 def hello_world(request):
+	return Response({"message": "Hello, world!"})
+
+@api_view(['GET'])
+def coucou(request):
 	return Response({"message": "Hello, world!"})
 	
