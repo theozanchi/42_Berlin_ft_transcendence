3b20dd27dc6194dd2e0a7e5946e0327cc95f336d
tzanchi <tzanchi@student.42berlin.de>
2024-06-20 11:21:35 +0200

    adding jakob's files

---
+++ frontend/js/player.js
@@ -0,0 +1,50 @@
+class MyPlayer extends HTMLElement {
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({mode: 'open'});
+	}
+
+	connectedCallback() {
+		this.render();
+		if (this.hasAttribute('remove-button'))
+			this.shadow.getElementById('removeButton').addEventListener('click', () => {
+				console.log("REMOVE CLICKED");
+				this.dispatchEvent(new CustomEvent('removePlayer'));
+			});
+	}
+
+	render () {
+		const	hasInput = this.hasAttribute('input');
+		const	hasRemoveButton = this.hasAttribute('remove-button');
+		const	baseUrl = document.location.href;
+		const	imageUrl = new URL('assets/avatar_blossom.png', baseUrl);
+		const	name = this.getAttribute('name');
+		if (this.hasAttribute('avatar'))
+			imageUrl = new URL(this.getAttribute('avatar'), baseUrl);
+
+		this.shadow.innerHTML = `
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+            <style>
+                img {
+                    height: 48px;
+                }
+				.d-flex > * {
+                    margin: 6px 6px;
+                }
+            </style>
+            <div class="d-flex align-items-center justify-content-center border-bottom">
+                <img src="${imageUrl}" class="col-auto">
+                ${hasInput 
+                    ? 
+                        '<input type="text" class="form-control col" maxlength="30" value="' + this.getAttribute('name') + '">' 
+                    : 
+                        `<p class="col"> ${name} </p>`
+                }
+                ${hasRemoveButton ? '<button id="removeButton" class="btn btn-outline-danger col-auto">X</button>' : ''}
+			<span class="border-bottom"></span>
+			</div>
+			`;
+	}
+}
+
+customElements.define('player-component', MyPlayer);
---
+++ frontend/js/stepper_titlebar.js
@@ -0,0 +1,35 @@
+// TITLE BAR THAT IS USED IN THE GAME SETUP STEPPER
+// CONSIST OF:
+	// BACK BUTTON
+	// TITLE
+
+class StepperTitleBar extends HTMLElement {
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({mode: 'open'});
+	}
+
+	connectedCallback() {
+		// console.log("rendering titlebar");
+		this.render();
+	}
+
+	render() {
+		this.shadow.innerHTML = `
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"></link>
+			<style>
+				.d-flex > * {
+                    margin: 6px 6px;
+                }
+				.btn-outline-secondary > * { width: 48px;}
+            </style>
+			<div class = "d-flex align-items-center justify-content-center border-bottom border-black border-2"> \
+					<button id="backButton" class="btn btn-outline-secondary col-auto">←</button> \
+					<h2 class="col text-truncate text-center" > ${this.getAttribute('title')} </h2> \
+			</div>
+			`
+	}
+}
+
+
+customElements.define('stepper-titlebar', StepperTitleBar);
