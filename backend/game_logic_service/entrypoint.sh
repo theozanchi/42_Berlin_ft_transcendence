44a6bc3683f88e056e71b3e13ea3af2c916dbfcd
tzanchi <tzanchi@student.42berlin.de>
2024-06-25 12:11:34 +0200

    removed entrypoint script and added everything in the Dockerfile

--- backend/game_logic_service/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python manage.py makemigrations game_logic
-python manage.py migrate
-python manage.py collectstatic --noinput
-
-echo "Starting Daphne"
-exec daphne -b 0.0.0.0 -p 8002 game_logic_service.asgi:application
