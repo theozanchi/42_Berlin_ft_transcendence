4513c8af4249bec9158866e9225ff0fd1856ed9c
tzanchi <tzanchi@student.42berlin.de>
2024-06-25 12:20:26 +0200

    added command to makemigrations and migrate to database

--- backend/game_config_service/game_config/settings.py
+++ backend/game_config_service/game_config/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/share/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
