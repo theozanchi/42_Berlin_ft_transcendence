a291faaa0cf55f5f7d94ff5c4fbe88d5b2d2d43d
M42 <mgraf@student.42berlin.de>
2024-06-26 13:25:50 +0200

    ongoing integration into docker compose

--- backend/user_mgt/user_mgt/settings.py
+++ backend/user_mgt/user_mgt/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
