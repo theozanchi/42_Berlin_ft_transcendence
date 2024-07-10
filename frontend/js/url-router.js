8ba73d330097d885a30e7df3f7b8debfffab4923
Jakob <jschott@student.42berlin.de>
2024-07-10 11:37:18 +0200

    some layout fixes

--- frontend/js/url-router.js
+++ frontend/js/url-router.js
@@ -79,8 +79,8 @@ const urlRoutes = {
 
 const urlRoute = (eventOrUrl) => {
     let url;
-	console.log(eventOrUrl);
-	console.log(typeof eventOrUrl);
+	// console.log(eventOrUrl);
+	// console.log(typeof eventOrUrl);
     if (typeof eventOrUrl === 'string') {
         url = eventOrUrl;
     } else {
@@ -88,7 +88,7 @@ const urlRoute = (eventOrUrl) => {
         eventOrUrl.preventDefault();
         url = eventOrUrl.target.href;
     }
-	console.log(url);
+	// console.log(url);
     window.history.pushState({}, "", url);
     urlLocationHandler();
 }
@@ -98,7 +98,9 @@ const urlLocationHandler = async () => {
     if (location.length == 0) {
         location = "/"
     }
-    // console.log(`MY LOCATION: ${location}`);
+	// console.log(`MY LOCATION: ${location}`);
+
+    // console.log(`MY LOCATION`);
 
     const route = urlRoutes[location] || urlRoutes[404]
 
@@ -115,7 +117,20 @@ const urlLocationHandler = async () => {
     let doc = parser.parseFromString(html, "text/html");
     let title = doc.querySelector('title').innerText;
 
-	let fetchedSettingsColumnContent = doc.getElementById('settings-column').innerHTML;
+	let fetchedSettingsColumnContent 
+	
+	if (location === '/game') {
+		// console.log("TRYING TO LAUNCH A GAME");
+		fetchedSettingsColumnContent = doc.getElementById('game-column').innerHTML;
+		//OVERWRITE COLUMN
+		document.getElementById("game-column").innerHTML = fetchedSettingsColumnContent;	
+	}
+	else {
+		fetchedSettingsColumnContent = doc.getElementById('settings-column').innerHTML;
+		//OVERWRITE COLUMN
+		document.getElementById("settings-column").innerHTML = fetchedSettingsColumnContent;	
+
+	}
 
 	
 	//WRITE NEW TITLE TO BROWSER TAB
@@ -126,8 +141,7 @@ const urlLocationHandler = async () => {
 	//OVERWRITE CONTENT
     // document.getElementById("content").innerHTML = html;
 
-	//OVERWRITE COLUMN
-	document.getElementById("settings-column").innerHTML = fetchedSettingsColumnContent;
+	
 };
 
 window.onpopstate = urlLocationHandler;
