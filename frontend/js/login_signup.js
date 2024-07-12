3ff60b291bd57232304322ea63b9a234819ba993
Jakob <jschott@student.42berlin.de>
2024-07-12 14:31:36 +0200

    some fixes for user

--- frontend/js/login_signup.js
+++ frontend/js/login_signup.js
@@ -1,19 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const emailInput = document.getElementById('loginEmail');
-    const passwordInput = document.getElementById('loginPassword');
-    const loginButton = document.getElementById('loginButton');
 
-    function enableOrDisableLoginButton() {
-        if (emailInput.value && passwordInput.value) {
-            loginButton.disabled = false;
-        } else {
-            loginButton.disabled = true;
-        }
-    }
 
-    emailInput.addEventListener('input', enableOrDisableLoginButton);
-    passwordInput.addEventListener('input', enableOrDisableLoginButton);
+/// LOGIN LISTENER ///
+// document.addEventListener('DOMContentLoaded', function() {
+//     const emailInput = document.getElementById('loginUser');
+//     const passwordInput = document.getElementById('loginPassword');
+//     const loginButton = document.getElementById('loginButton');
+
+//     function enableOrDisableLoginButton() {
+//         if (emailInput.value && passwordInput.value) {
+//             loginButton.disabled = false;
+//         } else {
+//             loginButton.disabled = true;
+//         }
+//     }
+
+//     emailInput.addEventListener('input', enableOrDisableLoginButton);
+//     passwordInput.addEventListener('input', enableOrDisableLoginButton);
  
-    // Initially disable the button
-    loginButton.disabled = true;
+//     // Initially disable the button
+//     loginButton.disabled = true;
+// });
+
+
+/// SIGN UP LISTENER
+document.addEventListener('DOMContentLoaded', function() {
+	const signupUser = document.getElementById('signupUser');
+	const loginPassword = document.getElementById('signupPassword');
+	const loginPasswordConfirm = document.getElementById('signupPasswordConfirm');
+	const loginButton = document.getElementById('signupButton');
+
+	console.log(signupUser + loginPassword + loginPasswordConfirm + loginButton);
+
+	function validateForm() {
+		if (signupUser.value && loginPassword.value && loginPasswordConfirm.value && loginPassword.value === loginPasswordConfirm.value) {
+			loginButton.disabled = false;
+		} else {
+			loginButton.disabled = true;
+		}
+	}
+
+	signupUser.addEventListener('input', validateForm);
+	loginPassword.addEventListener('input', validateForm);
+	loginPasswordConfirm.addEventListener('input', validateForm);
 });
