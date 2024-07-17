c0b846c553bef6f7c54b10631329ca608c145cee
M42 <mgraf@student.42berlin.de>
2024-07-17 13:36:05 +0200

    updated player names frontend integration

--- frontend/js/player.js
+++ frontend/js/player.js
@@ -8,33 +8,54 @@ class MyPlayer extends HTMLElement {
 		this.render();
 		if (this.hasAttribute('remove-button'))
 			this.shadow.getElementById('removeButton').addEventListener('click', () => {
-				console.log("REMOVE CLICKED");
+				// console.log("REMOVE CLICKED");
 				this.dispatchEvent(new CustomEvent('removePlayer'));
 			});
 	}
 
+    get name() {
+        return this.getAttribute('name');
+    }
+
+    set name(newValue) {
+        this.setAttribute('name', newValue);
+        this.render();
+    }
+
+    static get observedAttributes() {
+        return ['name'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'name' && oldValue !== newValue) {
+			console.log(`RERENDERING: ${name} ${oldValue}  ${newValue}`);
+            this.render();
+        }
+    }
+
 	render() {
 		const hasInput = this.hasAttribute('input');
 		const hasRemoveButton = this.hasAttribute('remove-button');
+		const hasAvatar = this.hasAttribute('avatar');
 		const rightToLeft = this.hasAttribute('order-right');
 		const tableColumn = this.getAttribute('table-column');
 		const baseUrl = document.location.href;
-		let imageUrl = new URL('assets/avatar_blossom.png', baseUrl);
 		const name = this.getAttribute('name');
+		let imageUrl = new URL('assets/avatar_blossom.png', baseUrl);
 	
-		if (this.hasAttribute('avatar'))
+		if (hasAvatar)
 			imageUrl = new URL(this.getAttribute('avatar'), baseUrl);
 	
 		const nameAligned = (tableColumn === "right")
-			? `<p class="col text-end">${name}</p>`
-			: `<p class="col">${name}</p>`
+			? `<p class="col align-middle fs-5 m-0 text-end">${name}</p>`
+			: `<p class="col align-middle fs-5 m-0">${name}</p>`
 
 		const inputElement = hasInput 
-			? `<input type="text" class="form-control col" maxlength="30" value="${name}">` 
+			? `<input id="playerNicknameInput" type="text" class="form-control col" maxlength="30" value="${name}">` 
 			: `${nameAligned}`;
 	
 		const removeButtonElement = hasRemoveButton 
-			? '<button id="removeButton" class="btn btn-outline-danger col-auto"><i class="bi bi-x-lg"></i></button>' 
+			? '<button id="removeButton" class="btn btn-outline-danger col-auto square-button"><i class="bi bi-x-lg"></i></button>' 
 			: '';
 	
 		const imgElement = `<img src="${imageUrl}" class="col-auto player-component">`;
@@ -43,12 +64,18 @@ class MyPlayer extends HTMLElement {
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 			<link rel="stylesheet" href="./css/styles.css">
-			<div class="player-component d-flex align-items-center justify-content-center">
+			<div class="player-component d-flex align-items-center">
 				${rightToLeft
-						? removeButtonElement + inputElement + imgElement 
+						? removeButtonElement + inputElement + imgElement
 						: imgElement + inputElement + removeButtonElement}
 			</div>
 		`;
+
+		if (hasInput) {
+			this.shadow.getElementById('playerNicknameInput').addEventListener('change', (event) => {
+				this.name = event.target.value;
+			});
+		}
 	}
 }
 
