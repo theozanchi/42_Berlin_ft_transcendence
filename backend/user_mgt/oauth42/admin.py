eaa264be3b4bb5e659f0749a641f78652bc14eda
M42 <mgraf@student.42berlin.de>
2024-07-22 14:12:39 +0200

    added rounds in admin panel

--- backend/user_mgt/oauth42/admin.py
+++ backend/user_mgt/oauth42/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from django.utils.html import format_html
 
-from .models import Participation, Game, Player
+from .models import Participation, Game, Player, Round
 
 
 class PlayerInline(admin.TabularInline):
@@ -99,6 +99,7 @@ admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.register(Game, TournamentAdmin)
 admin.site.register(Participation, ParticipationAdmin)
+admin.site.register(Round)
 
 
 admin.site.site_header = "Transcendence User Management"
