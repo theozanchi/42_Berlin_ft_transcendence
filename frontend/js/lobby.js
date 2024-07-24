c80f48bc1c02206db37690a2e7c4513d320e0f75
tzanchi <tzanchi@student.42berlin.de>
2024-07-24 14:23:21 +0200

    trying to fix the game lobby

--- frontend/js/lobby.js
+++ frontend/js/lobby.js
@@ -9,11 +9,25 @@ window.onload = function() {
 	// setTimeout(setGameID(), 3000);
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-	setGameID('test');
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+// 	setGameID('test');
+// });
 
-
+export function startGameButton() {
+	console.log('startGameButton called');
+	// document.addEventListener('DOMContentLoaded', (event) => {
+    const startGameButton = document.getElementById('startGameButton');
+	if (startGameButton)
+		console.log('startGameButton found in the DOM');
+	else
+		console.log('startGameButton not found in the DOM');
+    startGameButton.addEventListener('click', function() {
+		console.log('startGameButton event listener called');
+        sendJson(JSON.stringify({ type: 'start-game' }));
+        console.log('Start Game button clicked');
+    });
+	// });
+}
 
 function setGameID(gameID) {
 	if (!gameID) {
